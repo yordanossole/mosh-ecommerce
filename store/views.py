49172fe3abb97b39db06b424f5bbc86a63d61da5
Yordanos Solomon <yordanos.sole@gmail.com>
2025-02-02 23:02:54 +0300

    serializer formed and configured, status code is returned

--- store/views.py
+++ store/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from store.models import Customer
+from .models import Product
+from .serializers import ProductSerializer
+from django.shortcuts import get_object_or_404
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 def index(request):
@@ -12,8 +16,22 @@ def index(request):
 
 @api_view()
 def product_list(request):
-    return Response('Ok')
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
 
 @api_view()
 def product_detail(request, id):
-    return Response(id)
+    # try:
+    #     product = Product.objects.get(pk=id)
+    #     serializer = ProductSerializer(product)
+    #     return Response(serializer.data)
+    # except Product.DoesNotExist:
+    #     # return Response(status=404)
+    #     return Response(status=status.HTTP_404_NOT_FOUND)
+
+    # another way to do the same thing as above
+
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
