274c6e48d35dbe634cf5a2059b5e89b3f0e9d315
Yordanos Solomon <yordanos.sole@gmail.com>
2025-02-03 22:09:36 +0300

    just commit again for consistency in commiting

--- store/views.py
+++ store/views.py
@@ -1,41 +1,41 @@
-# from django.shortcuts import render
-# from store.models import Customer
-# from .models import Product
-# from .serializers import ProductSerializer
-# from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+from store.models import Customer
+from .models import Product
+from .serializers import ProductSerializer
+from django.shortcuts import get_object_or_404
 
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-# from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# # Create your views here.
-# def index(request):
-#     queryset = Customer.objects.all()
+# Create your views here.
+def index(request):
+    queryset = Customer.objects.all()
 
-#     return render(request, "index.html", { 'customers':list(queryset)})
+    return render(request, "index.html", { 'customers':list(queryset)})
 
-# @api_view()
-# def product_list(request):
-#     queryset = Product.objects.select_related('colloection').all()
-#     serializer = ProductSerializer(queryset, many=True, context={'request': request})
-#     return Response(serializer.data)
+@api_view()
+def product_list(request):
+    queryset = Product.objects.select_related('colloection').all()
+    serializer = ProductSerializer(queryset, many=True, context={'request': request})
+    return Response(serializer.data)
 
-# @api_view()
-# def product_detail(request, id):
-#     # try:
-#     #     product = Product.objects.get(pk=id)
-#     #     serializer = ProductSerializer(product)
-#     #     return Response(serializer.data)
-#     # except Product.DoesNotExist:
-#     #     # return Response(status=404)
-#     #     return Response(status=status.HTTP_404_NOT_FOUND)
+@api_view()
+def product_detail(request, id):
+    # try:
+    #     product = Product.objects.get(pk=id)
+    #     serializer = ProductSerializer(product)
+    #     return Response(serializer.data)
+    # except Product.DoesNotExist:
+    #     # return Response(status=404)
+    #     return Response(status=status.HTTP_404_NOT_FOUND)
 
-#     # another way to do the same thing as above
+    # another way to do the same thing as above
 
-#     product = get_object_or_404(Product, pk=id)
-#     serializer = ProductSerializer(product)
-#     return Response(serializer.data)
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
 
-# @api_view()
-# def collection_detail(request, pk):
-#     return Response('Ok')
+@api_view()
+def collection_detail(request, pk):
+    return Response('Ok')
