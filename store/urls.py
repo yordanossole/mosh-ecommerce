d33e24fd4853a4936866447e2656a9fa2d58cea1
yordanossole <yordanos.sole@gmail.com>
2025-03-26 20:54:36 +0300

    Collection detail and list vies implemented

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('products/', views.product_list),
     # path('products/<id>', views.product_detail)
     path('products/<int:id>', views.product_detail), # to make it only works for int id
+    path('collections/', views.collection_list),
     path('collections/<int:pk>', views.collection_detail, name='collection-detail')
 ]
