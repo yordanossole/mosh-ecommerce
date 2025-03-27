d8ee08882b6b42defb04aabb03e09d228d3dfdff
yordanossole <yordanos.sole@gmail.com>
2025-03-27 15:12:08 +0300

    Respond collection with link to the collection

--- store/serializers.py
+++ store/serializers.py
@@ -54,6 +54,10 @@ class ProductSerializer(serializers.ModelSerializer):
         # }
         
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    colloection = serializers.HyperlinkedRelatedField(
+        queryset = Collection.objects.all(),
+        view_name = 'collection-detail'
+    )
 
     def calculate_tax(self, product : Product):
         return product.unit_price * Decimal(1.1)
